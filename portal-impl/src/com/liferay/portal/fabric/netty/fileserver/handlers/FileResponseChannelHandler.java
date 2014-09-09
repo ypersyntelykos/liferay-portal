f481f855444da03da06c1143edb089c5e42a8508
shuyangzhou <shuyang.zhou@liferay.com>
2014-09-09 16:18:05 -0700

    File server support

---
+++ portal-impl/src/com/liferay/portal/fabric/netty/fileserver/handlers/FileResponseChannelHandler.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.fabric.netty.fileserver.handlers;
+
+import com.liferay.portal.fabric.netty.codec.serialization.ObjectDecodeChannelInboundHandler;
+import com.liferay.portal.fabric.netty.fileserver.FileResponse;
+import com.liferay.portal.kernel.concurrent.AsyncBroker;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.nio.file.Path;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class FileResponseChannelHandler
+	extends ObjectDecodeChannelInboundHandler<FileResponse> {
+
+	public FileResponseChannelHandler(
+		AsyncBroker<Path, FileResponse> asyncBroker,
+		EventExecutorGroup eventExecutorGroup) {
+
+		_asyncBroker = asyncBroker;
+		_eventExecutorGroup = eventExecutorGroup;
+	}
+
+	@Override
+	public FileResponse channelRead0(
+			ChannelHandlerContext channelHandlerContext,
+			FileResponse fileResponse, ByteBuf byteBuf)
+		throws Exception {
+
+		if (fileResponse.isFileNotFound() || fileResponse.isFileNotModified()) {
+			_asyncBroker.takeWithResult(fileResponse.getPath(), fileResponse);
+
+			return null;
+		}
+
+		ChannelPipeline channelPipeline = channelHandlerContext.pipeline();
+
+		channelPipeline.addFirst(
+			new FileUploadChannelHandler(
+				_asyncBroker, fileResponse, _eventExecutorGroup.next()));
+
+		channelPipeline.fireChannelRead(byteBuf.retain());
+
+		return null;
+	}
+
+	private final AsyncBroker<Path, FileResponse> _asyncBroker;
+	private final EventExecutorGroup _eventExecutorGroup;
+
+}
---
+++ portal-impl/src/com/liferay/portal/fabric/netty/fileserver/handlers/FileUploadChannelHandler.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.fabric.netty.fileserver.handlers;
+
+import com.liferay.portal.fabric.netty.fileserver.FileHelperUtil;
+import com.liferay.portal.fabric.netty.fileserver.FileResponse;
+import com.liferay.portal.kernel.concurrent.AsyncBroker;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelPipeline;
+import io.netty.util.concurrent.EventExecutor;
+
+import java.io.IOException;
+
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileTime;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class FileUploadChannelHandler extends ChannelInboundHandlerAdapter {
+
+	public FileUploadChannelHandler(
+			AsyncBroker<Path, FileResponse> asyncBroker,
+			FileResponse fileResponse, EventExecutor eventExecutor)
+		throws IOException {
+
+		if (asyncBroker == null) {
+			throw new NullPointerException("Async broker is null");
+		}
+
+		if (fileResponse == null) {
+			throw new NullPointerException("File response is null");
+		}
+
+		if (eventExecutor == null) {
+			throw new NullPointerException("Event executor is null");
+		}
+
+		if (fileResponse.getSize() < 1) {
+			throw new IllegalArgumentException(
+				"File response has no content for uploading");
+		}
+
+		this.asyncBroker = asyncBroker;
+		this.fileResponse = fileResponse;
+		this.eventExecutor = eventExecutor;
+
+		tempPath = Files.createTempFile(
+			FileUploadChannelHandler.class.getName() + "-", null);
+
+		fileChannel = FileChannel.open(
+			tempPath, StandardOpenOption.WRITE,
+			StandardOpenOption.TRUNCATE_EXISTING);
+
+		fileResponse.setLocalFile(tempPath);
+	}
+
+	@Override
+	public void channelRead(
+			ChannelHandlerContext channelHandlerContext, Object object)
+		throws Exception {
+
+		ByteBuf byteBuf = (ByteBuf)object;
+
+		if (!receive(byteBuf)) {
+			return;
+		}
+
+		fileChannel.close();
+
+		if (eventExecutor.inEventLoop() || !fileResponse.isFolder()) {
+			finish(fileResponse.getPath(), fileResponse);
+		}
+		else {
+			eventExecutor.submit(new Callable<Void>() {
+
+				@Override
+				public Void call() throws Exception {
+					try {
+						finish(fileResponse.getPath(), fileResponse);
+					}
+					catch (IOException ioe) {
+						exceptionCaught(null, ioe);
+					}
+
+					return null;
+				}
+
+			});
+		}
+
+		ChannelPipeline channelPipeline = channelHandlerContext.pipeline();
+
+		channelPipeline.remove(this);
+
+		if (byteBuf.isReadable()) {
+			channelHandlerContext.fireChannelRead(object);
+		}
+	}
+
+	@Override
+	public void exceptionCaught(
+			ChannelHandlerContext channelHandlerContext, Throwable throwable)
+		throws IOException {
+
+		_log.error("File upload failure", throwable);
+
+		if (channelHandlerContext != null) {
+			ChannelPipeline channelPipeline = channelHandlerContext.pipeline();
+
+			channelPipeline.remove(this);
+		}
+
+		asyncBroker.takeWithException(fileResponse.getPath(), throwable);
+
+		fileChannel.close();
+
+		Files.delete(tempPath);
+	}
+
+	protected void finish(Path path, FileResponse fileResponse)
+		throws IOException {
+
+		if (fileResponse.isFolder()) {
+			fileResponse.setLocalFile(FileHelperUtil.unzip(tempPath));
+
+			Files.delete(tempPath);
+		}
+
+		Files.setLastModifiedTime(
+			fileResponse.getLocalFile(),
+			FileTime.fromMillis(fileResponse.getLastModified()));
+
+		asyncBroker.takeWithResult(path, fileResponse);
+	}
+
+	protected boolean receive(ByteBuf byteBuf) throws IOException {
+		while (true) {
+			long readSize = fileResponse.getSize() - fileChannel.position();
+
+			int readableBytes = byteBuf.readableBytes();
+
+			if (readSize > readableBytes) {
+				readSize = readableBytes;
+			}
+
+			if (byteBuf.readBytes(fileChannel, (int)readSize) == readSize) {
+				break;
+			}
+		}
+
+		if (!byteBuf.isReadable()) {
+			byteBuf.release();
+		}
+
+		if (fileChannel.position() < fileResponse.getSize()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	protected final AsyncBroker<Path, FileResponse> asyncBroker;
+	protected final EventExecutor eventExecutor;
+	protected final FileChannel fileChannel;
+	protected final FileResponse fileResponse;
+	protected final Path tempPath;
+
+	private static Log _log = LogFactoryUtil.getLog(
+		FileUploadChannelHandler.class);
+
+}
