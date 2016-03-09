2939f548b340d5e2467a82fb0a88100e7a615c73
tina.tian <tina.tian@liferay.com>
2016-03-09 09:48:36 +0800

    LPS-63617 More try-with-resources, com.liferay.portal.upgrade.v6_2_0

--- portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeCustomizablePortlets.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_2_0/UpgradeCustomizablePortlets.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.upgrade.v6_2_0;
 
-import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.model.CustomizedPages;
 import com.liferay.portal.kernel.model.LayoutTypePortletConstants;
 import com.liferay.portal.kernel.model.PortletConstants;
@@ -42,16 +41,11 @@ public class UpgradeCustomizablePortlets extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			ps = connection.prepareStatement(
+		try (PreparedStatement ps = connection.prepareStatement(
 				"select ownerId, ownerType, preferences from " +
 					"PortalPreferences where preferences like " +
 						"'%com.liferay.portal.model.CustomizedPages%'");
-
-			rs = ps.executeQuery();
+			ResultSet rs = ps.executeQuery()) {
 
 			while (rs.next()) {
 				long ownerId = rs.getLong("ownerId");
@@ -67,9 +61,6 @@ public class UpgradeCustomizablePortlets extends UpgradeProcess {
 
 				portalPreferencesWrapper.store();
 			}
-		}
-		finally {
-			DataAccess.cleanUp(ps, rs);
 		}
 	}
 
@@ -87,41 +78,35 @@ public class UpgradeCustomizablePortlets extends UpgradeProcess {
 			long ownerId, int ownerType, long plid, String portletId)
 		throws Exception {
 
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			ps = connection.prepareStatement(
+		try (PreparedStatement ps = connection.prepareStatement(
 				"select portletPreferencesId, ownerId, ownerType, plid, " +
 					"portletId, preferences from PortletPreferences where " +
-						"ownerId = ?, ownerType = ?, plid = ?, portletId = ?");
+						"ownerId = ?, ownerType = ?, plid = ?, portletId = " +
+							"?")) {
 
 			ps.setLong(1, ownerId);
 			ps.setInt(2, ownerType);
 			ps.setLong(3, plid);
 			ps.setString(4, portletId);
 
-			rs = ps.executeQuery();
+			try (ResultSet rs = ps.executeQuery()) {
+				if (!rs.next()) {
+					return null;
+				}
 
-			if (!rs.next()) {
-				return null;
+				PortletPreferences portletPreferences =
+					new PortletPreferencesImpl();
+
+				portletPreferences.setPortletPreferencesId(
+					rs.getLong("portletPreferencesId"));
+				portletPreferences.setOwnerId(rs.getLong("ownerId"));
+				portletPreferences.setOwnerType(rs.getInt("ownerType"));
+				portletPreferences.setPlid(rs.getLong("plid"));
+				portletPreferences.setPortletId(rs.getString("portletId"));
+				portletPreferences.setPreferences(rs.getString("preferences"));
+
+				return portletPreferences;
 			}
-
-			PortletPreferences portletPreferences =
-				new PortletPreferencesImpl();
-
-			portletPreferences.setPortletPreferencesId(
-				rs.getLong("portletPreferencesId"));
-			portletPreferences.setOwnerId(rs.getLong("ownerId"));
-			portletPreferences.setOwnerType(rs.getInt("ownerType"));
-			portletPreferences.setPlid(rs.getLong("plid"));
-			portletPreferences.setPortletId(rs.getString("portletId"));
-			portletPreferences.setPreferences(rs.getString("preferences"));
-
-			return portletPreferences;
-		}
-		finally {
-			DataAccess.cleanUp(ps, rs);
 		}
 	}
 
@@ -147,14 +132,10 @@ public class UpgradeCustomizablePortlets extends UpgradeProcess {
 			long userId, long plid, String portletId, String newPortletId)
 		throws Exception {
 
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			ps = connection.prepareStatement(
+		try (PreparedStatement ps = connection.prepareStatement(
 				"update PortletPreferences set ownerId = ?, ownerType = ?, " +
 					"plid = ?, portletId = ? where ownerId = ? and " +
-						"ownerType = ? and plid = ? and portletId = ?");
+						"ownerType = ? and plid = ? and portletId = ?")) {
 
 			ps.setLong(1, userId);
 			ps.setInt(2, PortletKeys.PREFS_OWNER_TYPE_USER);
@@ -166,9 +147,6 @@ public class UpgradeCustomizablePortlets extends UpgradeProcess {
 			ps.setString(8, portletId);
 
 			ps.executeUpdate();
-		}
-		finally {
-			DataAccess.cleanUp(ps, rs);
 		}
 	}
 
