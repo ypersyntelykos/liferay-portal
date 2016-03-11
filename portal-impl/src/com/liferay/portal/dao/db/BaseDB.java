9fb427d54c043be5706afb5fa5d92fc4fc76edf9
shuyangzhou <shuyang.zhou@liferay.com>
2016-03-10 16:11:36 -0800

    LPS-62450 Proper naming

--- portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
+++ portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -927,7 +927,7 @@ public abstract class BaseDB implements DB {
 
 			while ((line = unsyncBufferedReader.readLine()) != null) {
 				if (!line.startsWith(comments)) {
-					line = StringUtil.removeSubstrings(
+					line = StringUtil.removeChars(
 						line, new char[] {'\n', '\t'});
 
 					if (line.endsWith(";")) {
