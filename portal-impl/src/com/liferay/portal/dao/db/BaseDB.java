ypersyntelykos/liferay-portal

commit 24e24c3a0e1af3deb656388bd8166ce0b2b2847d

LPS-63254 If a line is ended with 'go', before the keyword should be writen a space, otherwise a word like 'logo' or similars can be used as end of lines

portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -274,8 +274,8 @@ public abstract class BaseDB implements DB {
 					sql = sql.substring(0, sql.length() - 1);
 				}
 
-				if (sql.endsWith("go")) {
-					sql = sql.substring(0, sql.length() - 2);
+				if (sql.endsWith(" go")) {
+					sql = sql.substring(0, sql.length() - 3);
 				}
 
 				if (_log.isDebugEnabled()) {

commit c46723e6697012ea8cc4118b7aab5baabef98c96

LPS-63254 Fix regression caused by 24e24c3a0e1af3deb656388bd8166ce0b2b2847d, this is causing portal fails to start on empty Sybase table. See SybaseDB.buildSQL() for the way how it replaces/appends the "go" keyword.

portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -279,7 +279,7 @@ public abstract class BaseDB implements DB {
 					sql = sql.substring(0, sql.length() - 1);
 				}
 
-				if (sql.endsWith(" go")) {
+				if (sql.endsWith("\ngo")) {
 					sql = sql.substring(0, sql.length() - 3);
 				}
 