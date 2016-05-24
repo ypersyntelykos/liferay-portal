db14cf757a0a810c8b145f40457e9718f995ad2d
Michael Bowerman <michael.bowerman@liferay.com>
2016-05-24 15:40:13 -0700

    LPS-66047 Clear the list of any prior resource actions before reading an xml file to add new resource actions in case the user removed those actions from the xml file

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -1176,6 +1176,8 @@ public class ResourceActionsImpl implements ResourceActions {
 		Set<String> modelResourceActions =
 			modelResourceActionsBag.getResourceActions();
 
+		modelResourceActions.clear();
+
 		readSupportsActions(modelResourceElement, modelResourceActions);
 
 		checkModelActions(modelResourceActions);
@@ -1242,6 +1244,8 @@ public class ResourceActionsImpl implements ResourceActions {
 
 		Set<String> portletResourceActions =
 			portletResourceActionsBag.getResourceActions();
+
+		portletResourceActions.clear();
 
 		readSupportsActions(portletResourceElement, portletResourceActions);
 
