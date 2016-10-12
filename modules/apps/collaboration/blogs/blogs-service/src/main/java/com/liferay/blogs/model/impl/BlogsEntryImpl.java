538c69e499a477ed5c38dd1259ca2214230dd494
shuyangzhou <shuyang.zhou@liferay.com>
2016-10-11 17:08:33 -0700

    Copy over BlogsEntryImpl body

--- modules/apps/collaboration/blogs/blogs-service/src/main/java/com/liferay/blogs/model/impl/BlogsEntryImpl.java
+++ modules/apps/collaboration/blogs/blogs-service/src/main/java/com/liferay/blogs/model/impl/BlogsEntryImpl.java
@@ -16,6 +16,19 @@ package com.liferay.blogs.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.document.library.kernel.util.DLUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Image;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ImageLocalServiceUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.webserver.WebServerServletTokenUtil;
+
+import java.util.Date;
+
 /**
  * The extended model implementation for the BlogsEntry service. Represents a row in the &quot;BlogsEntry&quot; database table, with each column mapped to a property of this class.
  *
@@ -27,11 +40,104 @@ import aQute.bnd.annotation.ProviderType;
  */
 @ProviderType
 public class BlogsEntryImpl extends BlogsEntryBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a blogs entry model instance should use the {@link com.liferay.blogs.model.BlogsEntry} interface instead.
-	 */
-	public BlogsEntryImpl() {
+
+	@Override
+	public String getCoverImageURL(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		long coverImageFileEntryId = getCoverImageFileEntryId();
+
+		if (coverImageFileEntryId == 0) {
+			return null;
+		}
+
+		FileEntry fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
+			coverImageFileEntryId);
+
+		return DLUtil.getPreviewURL(
+			fileEntry, fileEntry.getFileVersion(), themeDisplay,
+			StringPool.BLANK);
 	}
+
+	/**
+	 * @deprecated As of 1.1.0, replaced by {@link
+	 *             #getSmallImageURL(ThemeDisplay)}
+	 */
+	@Deprecated
+	@Override
+	public String getEntryImageURL(ThemeDisplay themeDisplay) {
+		if (!isSmallImage()) {
+			return null;
+		}
+
+		if (Validator.isNotNull(getSmallImageURL())) {
+			return getSmallImageURL();
+		}
+
+		return themeDisplay.getPathImage() + "/blogs/entry?img_id=" +
+			getSmallImageId() + "&t=" +
+				WebServerServletTokenUtil.getToken(getSmallImageId());
+	}
+
+	@Override
+	public String getSmallImageType() throws PortalException {
+		if ((_smallImageType == null) && isSmallImage()) {
+			Image smallImage = ImageLocalServiceUtil.getImage(
+				getSmallImageId());
+
+			_smallImageType = smallImage.getType();
+		}
+
+		return _smallImageType;
+	}
+
+	@Override
+	public String getSmallImageURL(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		if (Validator.isNotNull(getSmallImageURL())) {
+			return getSmallImageURL();
+		}
+
+		long smallImageFileEntryId = getSmallImageFileEntryId();
+
+		if (smallImageFileEntryId != 0) {
+			FileEntry fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
+				smallImageFileEntryId);
+
+			return DLUtil.getPreviewURL(
+				fileEntry, fileEntry.getFileVersion(), themeDisplay,
+				StringPool.BLANK);
+		}
+
+		long smallImageId = getSmallImageId();
+
+		if (smallImageId != 0) {
+			return themeDisplay.getPathImage() + "/blogs/entry?img_id=" +
+				getSmallImageId() + "&t=" +
+					WebServerServletTokenUtil.getToken(getSmallImageId());
+		}
+
+		return getCoverImageURL(themeDisplay);
+	}
+
+	@Override
+	public boolean isVisible() {
+		Date displayDate = getDisplayDate();
+
+		if (isApproved() && displayDate.before(new Date())) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	@Override
+	public void setSmallImageType(String smallImageType) {
+		_smallImageType = smallImageType;
+	}
+
+	private String _smallImageType;
+
 }
