ff55d97c1e116c6b5c67b938ffc4e4d1d5a5eef8
shuyangzhou <shuyang.zhou@liferay.com>
2016-10-21 17:14:40 -0700

    LPS-X Regenerate for journal

--- modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/service/persistence/test/JournalArticleLocalizationPersistenceTest.java
+++ modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/service/persistence/test/JournalArticleLocalizationPersistenceTest.java
@@ -28,7 +28,6 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.TransactionalTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.util.OrderByComparator;
@@ -36,6 +35,7 @@ import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PersistenceTestRule;
+import com.liferay.portal.test.rule.TransactionalTestRule;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -65,7 +65,8 @@ public class JournalArticleLocalizationPersistenceTest {
 	@Rule
 	public static final AggregateTestRule aggregateTestRule = new AggregateTestRule(new LiferayIntegrationTestRule(),
 			PersistenceTestRule.INSTANCE,
-			new TransactionalTestRule(Propagation.REQUIRED));
+			new TransactionalTestRule(Propagation.REQUIRED,
+				"com.liferay.journal.service"));
 
 	@Before
 	public void setUp() {
