ypersyntelykos/liferay-portal

commit b0a333424dc63b35afff33e3460548779586f0ac

LPS-67200 Create extension point for excluded navigation links and port to new configuration pattern

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/internal/servlet/taglib/util/SPAUtil.java
@@ -15,8 +15,8 @@
 package com.liferay.frontend.js.spa.web.internal.servlet.taglib.util;
 
 import com.liferay.frontend.js.spa.web.configuration.SPAConfiguration;
-import com.liferay.frontend.js.spa.web.configuration.SPAConfigurationActivator;
 import com.liferay.frontend.js.spa.web.configuration.SPAConfigurationUtil;
+import com.liferay.osgi.util.StringPlus;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -25,11 +25,13 @@ import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.servlet.ServletResponseConstants;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.util.Validator;
@@ -37,34 +39,42 @@ import com.liferay.portal.kernel.util.WebKeys;
 
 import java.lang.reflect.Field;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Filter;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Bruno Basto
  */
-@Component(service = SPAUtil.class)
+@Component(
+	configurationPid = "com.liferay.frontend.js.spa.web.configuration.SPAConfiguration",
+	service = SPAUtil.class
+)
+@Designate(ocd = SPAConfiguration.class)
 public class SPAUtil {
 
 	public long getCacheExpirationTime(long companyId) {
-		long cacheExpirationTime = 0;
-
-		SPAConfiguration spaConfiguration =
-			_spaConfigurationActivator.getSPAConfiguration();
-
-		cacheExpirationTime = GetterUtil.getLong(
-			spaConfiguration.cacheExpirationTime(), cacheExpirationTime);
+		long cacheExpirationTime = _spaConfiguration.cacheExpirationTime();
 
 		if (cacheExpirationTime > 0) {
 			cacheExpirationTime *= Time.MINUTE;
@@ -98,6 +108,11 @@ public class SPAUtil {
 		return ParamUtil.getString(request, portletNamespace + "redirect");
 	}
 
+	public String getNavigationExceptionSelectors() {
+		return ListUtil.toString(
+			_navigationExceptionSelectors, (String)null, StringPool.BLANK);
+	}
+
 	public String getPortletsBlacklist(ThemeDisplay themeDisplay) {
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 		List<Portlet> companyPortlets = _portletLocalService.getPortlets(
@@ -116,28 +131,11 @@ public class SPAUtil {
 	}
 
 	public int getRequestTimeout() {
-		int requestTimeout = 0;
-
-		SPAConfiguration spaConfiguration =
-			_spaConfigurationActivator.getSPAConfiguration();
-
-		requestTimeout = GetterUtil.getInteger(
-			spaConfiguration.requestTimeout(), requestTimeout);
-
-		return requestTimeout;
+		return _spaConfiguration.requestTimeout();
 	}
 
 	public int getUserNotificationTimeout() {
-		int userNotificationTimeout = 0;
-
-		SPAConfiguration spaConfiguration =
-			_spaConfigurationActivator.getSPAConfiguration();
-
-		userNotificationTimeout = GetterUtil.getInteger(
-			spaConfiguration.userNotificationTimeout(),
-			userNotificationTimeout);
-
-		return userNotificationTimeout;
+		return _spaConfiguration.userNotificationTimeout();
 	}
 
 	public String getValidStatusCodes() {
@@ -170,6 +168,43 @@ public class SPAUtil {
 		return false;
 	}
 
+	@Activate
+	protected void activate(
+			BundleContext bundleContext, SPAConfiguration spaConfiguration)
+		throws InvalidSyntaxException {
+
+		_spaConfiguration = spaConfiguration;
+
+		_navigationExceptionSelectors.addAll(
+			Arrays.asList(_spaConfiguration.navigationExceptionSelectors()));
+
+		Filter filter = bundleContext.createFilter(
+			"(&(objectClass=java.lang.Object)(" +
+				_SPA_NAVIGATION_EXCEPTION_SELECTOR_KEY + "=*))");
+
+		_navigationExceptionSelectorTracker = new ServiceTracker<>(
+			bundleContext, filter,
+			new NavigationExceptionSelectorTrackerCustomizer(bundleContext));
+
+		_navigationExceptionSelectorTracker.open();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_navigationExceptionSelectorTracker.close();
+	}
+
+	@Modified
+	protected void modified(SPAConfiguration spaConfiguration) {
+		_navigationExceptionSelectors.removeAll(
+			Arrays.asList(_spaConfiguration.navigationExceptionSelectors()));
+
+		_spaConfiguration = spaConfiguration;
+
+		_navigationExceptionSelectors.addAll(
+			Arrays.asList(_spaConfiguration.navigationExceptionSelectors()));
+	}
+
 	@Reference(unbind = "-")
 	protected void setPortletLocalService(
 		PortletLocalService portletLocalService) {
@@ -177,24 +212,13 @@ public class SPAUtil {
 		_portletLocalService = portletLocalService;
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.OPTIONAL,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void setSPAConfigurationActivator(
-		SPAConfigurationActivator spaConfigurationActivator) {
-
-		_spaConfigurationActivator = spaConfigurationActivator;
-	}
-
-	protected void unsetSPAConfigurationActivator(
-		SPAConfigurationActivator spaConfigurationActivator) {
-
-		_spaConfigurationActivator = null;
-	}
+	private static final String _SPA_NAVIGATION_EXCEPTION_SELECTOR_KEY =
+		"javascript.single.page.application.navigation.exception.selector";
 
 	private static final String _VALID_STATUS_CODES;
+
+	private static final List<String> _navigationExceptionSelectors =
+		new CopyOnWriteArrayList<>();
 
 	static {
 		Class<?> clazz = ServletResponseConstants.class;
@@ -212,7 +236,61 @@ public class SPAUtil {
 		_VALID_STATUS_CODES = jsonArray.toJSONString();
 	}
 
+	private ServiceTracker<Object, Object> _navigationExceptionSelectorTracker;
 	private PortletLocalService _portletLocalService;
-	private SPAConfigurationActivator _spaConfigurationActivator;
+	private SPAConfiguration _spaConfiguration;
+
+	private static final class NavigationExceptionSelectorTrackerCustomizer
+		implements ServiceTrackerCustomizer<Object, Object> {
+
+		public NavigationExceptionSelectorTrackerCustomizer(
+			BundleContext bundleContext) {
+
+			_bundleContext = bundleContext;
+		}
+
+		@Override
+		public Object addingService(ServiceReference<Object> reference) {
+			List<String> selectors = StringPlus.asList(reference.getProperty(
+				_SPA_NAVIGATION_EXCEPTION_SELECTOR_KEY));
+
+			Collections.addAll(
+				_navigationExceptionSelectors, selectors.toArray(new String[selectors.size()]));
+
+			Object service = _bundleContext.getService(reference);
+
+			_serviceReferences.add(reference);
+
+			return service;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<Object> reference, Object service) {
+
+			removedService(reference, service);
+
+			addingService(reference);
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<Object> reference, Object service) {
+
+			List<String> selectors = StringPlus.asList(reference.getProperty(
+				_SPA_NAVIGATION_EXCEPTION_SELECTOR_KEY));
+
+			_navigationExceptionSelectors.remove(selectors);
+
+			_serviceReferences.remove(reference);
+
+			_bundleContext.ungetService(reference);
+		}
+
+		private final BundleContext _bundleContext;
+		private final List<ServiceReference<Object>> _serviceReferences =
+			new CopyOnWriteArrayList<>();
+
+	}
 
 }

modules/apps/foundation/frontend-js/frontend-js-spa-web/build.gradle
@@ -27,11 +27,13 @@ configJSModules {
 
 dependencies {
 	provided group: "biz.aQute.bnd", name: "biz.aQute.bndlib", version: "3.1.0"
+	provided group: "com.liferay", name: "com.liferay.osgi.util", version: "3.0.0"
 	provided group: "com.liferay", name: "com.liferay.portal.configuration.metatype", version: "2.0.0"
 	provided group: "com.liferay.portal", name: "com.liferay.portal.kernel", version: "2.0.0"
 	provided group: "com.liferay.portal", name: "com.liferay.util.taglib", version: "2.0.0"
 	provided group: "javax.portlet", name: "portlet-api", version: "2.0"
 	provided group: "javax.servlet", name: "javax.servlet-api", version: "3.0.1"
+	provided group: "org.osgi", name: "org.osgi.core", version: "6.0.0"
 	provided group: "org.osgi", name: "org.osgi.service.component.annotations", version: "1.3.0"
 
 	senna group: "com.liferay.webjars", name: "com.liferay.webjars.senna.js", transitive: false, version: sennaVersion

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/configuration/SPAConfiguration.java
@@ -18,33 +18,43 @@ import aQute.bnd.annotation.metatype.Meta;
 
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
 
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
 /**
  * @author Bruno Basto
  */
 @ExtendedObjectClassDefinition(category = "foundation")
-@Meta.OCD(
+@ObjectClassDefinition(
 	id = "com.liferay.frontend.js.spa.web.configuration.SPAConfiguration",
 	localization = "content/Language", name = "spa.configuration.name"
 )
-public interface SPAConfiguration {
+public @interface SPAConfiguration {
 
 	@Meta.AD(
-		deflt = "-1", description = "cache.expiration.time.description",
-		name = "cache.expiration.time.name", required = true
+		description = "cache.expiration.time.description",
+		name = "cache.expiration.time.name", required = false
 	)
-	public String cacheExpirationTime();
+	public long cacheExpirationTime() default -1;
 
 	@Meta.AD(
-		deflt = "${server-property://com.liferay.portal/javascript.single.page.application.timeout}",
+		description = "navigation.exception.selectors.description",
+		name = "navigation.exception.selectors.name", required = false
+	)
+	public String[] navigationExceptionSelectors() default {
+		":not([target=\"_blank\"])", ":not([data-senna-off])",
+		":not([data-resource-href])"
+	};
+
+	@Meta.AD(
 		description = "request.timeout.description",
-		name = "request.timeout.name", required = true
+		name = "request.timeout.name", required = false
 	)
-	public String requestTimeout();
+	public int requestTimeout() default 0;
 
 	@Meta.AD(
-		deflt = "30000", description = "user.notification.timeout.description",
-		name = "user.notification.timeout.name", required = true
+		description = "user.notification.timeout.description",
+		name = "user.notification.timeout.name", required = false
 	)
-	public String userNotificationTimeout();
+	public int userNotificationTimeout() default 30000;
 
 }

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/configuration/SPAConfigurationActivator.java
@@ -1,59 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
-package com.liferay.frontend.js.spa.web.configuration;
-
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
-
-import java.util.Map;
-
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Modified;
-
-/**
- * @author Bruno Basto
- */
-@Component(
-	configurationPid = "com.liferay.frontend.js.spa.web.configuration.SPAConfiguration",
-	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true,
-	property = {
-		"cacheExpirationTime=-1", "requestTimeout=0",
-		"userNotificationTimeout=30000"
-	},
-	service = SPAConfigurationActivator.class
-)
-public class SPAConfigurationActivator {
-
-	public SPAConfiguration getSPAConfiguration() {
-		return _spaConfiguration;
-	}
-
-	@Activate
-	@Modified
-	protected void activate(Map<String, Object> properties) {
-		_spaConfiguration = ConfigurableUtil.createConfigurable(
-			SPAConfiguration.class, properties);
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_spaConfiguration = null;
-	}
-
-	private volatile SPAConfiguration _spaConfiguration;
-
-}

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/resources/META-INF/resources/init.jsp
@@ -34,6 +34,7 @@ SPAUtil spaUtil = (SPAUtil)request.getAttribute(SPAWebKeys.SPA_UTIL);
 	Liferay.SPA.cacheExpirationTime = <%= spaUtil.getCacheExpirationTime(themeDisplay.getCompanyId()) %>;
 	Liferay.SPA.clearScreensCache = <%= spaUtil.isClearScreensCache(request, session) %>;
 	Liferay.SPA.excludedPaths = <%= spaUtil.getExcludedPaths() %>;
+	Liferay.SPA.navigationExceptionSelectors = '<%= spaUtil.getNavigationExceptionSelectors() %>';
 	Liferay.SPA.loginRedirect = '<%= HtmlUtil.escapeJS(spaUtil.getLoginRedirect(request)) %>';
 	Liferay.SPA.requestTimeout = <%= spaUtil.getRequestTimeout() %>;
 	Liferay.SPA.userNotification = {

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/app/App.es.js
@@ -19,7 +19,7 @@ class LiferayApp extends App {
 		this.timeout = Math.max(Liferay.SPA.requestTimeout, 0) || Utils.getMaxTimeout();
 		this.timeoutAlert = null;
 
-		var exceptionsSelector = ':not([target="_blank"]):not([data-senna-off]):not([data-resource-href])';
+		var exceptionsSelector = Liferay.SPA.navigationExceptionSelectors;
 
 		this.setFormSelector('form' + exceptionsSelector);
 		this.setLinkSelector('a' + exceptionsSelector);

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/resources/content/Language.properties
@@ -1,6 +1,8 @@
 cache.expiration.time.description=Define the time, in minutes, in which the SPA cache is cleared. Use a zero value if the cache should never expire during SPA navigation. Use a negative value if cache should be disabled.
 cache.expiration.time.name=Cache Expiration Time
 it-looks-like-this-is-taking-longer-than-expected=It looks like this is taking longer than expected.
+navigation.exception.selectors.name=Navigation Exception Selectors
+navigation.exception.selectors.description=Define the CSS selectors that SPA should ignore
 oops=Oops
 request.timeout.description=Define the time, in milliseconds, in which a SPA request times out. Use a zero value if the request should never timeout.
 request.timeout.name=Request Timeout Time

commit d371928a6c6ed6b2552569bbca96fbc9985d3740

LPS-69736 No way this can be right, _navigationExceptionSelectors is a list of String, not a list of List<String>, caused by b0a333424dc63b35afff33e3460548779586f0ac

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/internal/servlet/taglib/util/SPAUtil.java
@@ -284,7 +284,7 @@ public class SPAUtil {
 			List<String> selectors = StringPlus.asList(
 				reference.getProperty(_SPA_NAVIGATION_EXCEPTION_SELECTOR_KEY));
 
-			_navigationExceptionSelectors.remove(selectors);
+			_navigationExceptionSelectors.removeAll(selectors);
 
 			_serviceReferences.remove(reference);
 