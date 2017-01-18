ypersyntelykos/liferay-portal

commit 6c12b7b88488be357afac1842e9943c487d7af22

LPS-36287 Remove unnecessary uses of NoSuchUserException try/catch

portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1146,58 +1146,56 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			return 0;
 		}
 
-		try {
-			User user = null;
+		User user = null;
 
-			if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
-				user = getUserByEmailAddress(companyId, login);
-			}
-			else if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
-				user = getUserByScreenName(companyId, login);
-			}
-			else if (authType.equals(CompanyConstants.AUTH_TYPE_ID)) {
-				user = getUserById(companyId, GetterUtil.getLong(login));
-			}
-
-			if (user.isDefaultUser()) {
-				if (_log.isInfoEnabled()) {
-					_log.info(
-						"Basic authentication is disabled for the default " +
-							"user");
-				}
-
-				return 0;
-			}
-			else if (!user.isActive()) {
-				if (_log.isInfoEnabled()) {
-					_log.info(
-						"Basic authentication is disabled for inactive user " +
-							user.getUserId());
-				}
-
-				return 0;
-			}
-
-			if (!PropsValues.BASIC_AUTH_PASSWORD_REQUIRED) {
-				return user.getUserId();
-			}
-
-			String userPassword = user.getPassword();
-
-			if (!user.isPasswordEncrypted()) {
-				userPassword = PasswordEncryptorUtil.encrypt(userPassword);
-			}
-
-			String encPassword = PasswordEncryptorUtil.encrypt(
-				password, userPassword);
-
-			if (userPassword.equals(password) ||
-				userPassword.equals(encPassword)) {
-
-				return user.getUserId();
-			}
+		if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
+			user = fetchUserByEmailAddress(companyId, login);
 		}
-		catch (NoSuchUserException nsue) {
+		else if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
+			user = fetchUserByScreenName(companyId, login);
+		}
+		else if (authType.equals(CompanyConstants.AUTH_TYPE_ID)) {
+			user = userPersistence.fetchByPrimaryKey(GetterUtil.getLong(login));
+		}
+
+		if (user == null) {
+			return 0;
+		}
+
+		if (user.isDefaultUser()) {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Basic authentication is disabled for the default " +
+						"user");
+			}
+
+			return 0;
+		}
+		else if (!user.isActive()) {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Basic authentication is disabled for inactive user " +
+						user.getUserId());
+			}
+
+			return 0;
+		}
+
+		if (!PropsValues.BASIC_AUTH_PASSWORD_REQUIRED) {
+			return user.getUserId();
+		}
+
+		String userPassword = user.getPassword();
+
+		if (!user.isPasswordEncrypted()) {
+			userPassword = PasswordEncryptorUtil.encrypt(userPassword);
+		}
+
+		String encPassword = PasswordEncryptorUtil.encrypt(
+			password, userPassword);
+
+		if (userPassword.equals(password) || userPassword.equals(encPassword)) {
+			return user.getUserId();
 		}
 
 		return 0;
@@ -1234,29 +1232,19 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		// Get User
 
-		User user = null;
+		User user = fetchUserByEmailAddress(companyId, username);
 
-		try {
-			user = getUserByEmailAddress(companyId, username);
-		}
-		catch (NoSuchUserException nsue) {
+		if (user == null) {
+			user = fetchUserByScreenName(companyId, username);
 		}
 
 		if (user == null) {
-			try {
-				user = getUserByScreenName(companyId, username);
-			}
-			catch (NoSuchUserException nsue) {
-			}
+			user = userPersistence.fetchByPrimaryKey(
+				GetterUtil.getLong(username));
 		}
 
 		if (user == null) {
-			try {
-				user = getUserById(GetterUtil.getLong(username));
-			}
-			catch (NoSuchUserException nsue) {
-				return 0;
-			}
+			return 0;
 		}
 
 		if (user.isDefaultUser()) {
@@ -5290,19 +5278,17 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		User user = null;
 
-		try {
-			if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
-				user = userPersistence.findByC_EA(companyId, login);
-			}
-			else if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
-				user = userPersistence.findByC_SN(companyId, login);
-			}
-			else if (authType.equals(CompanyConstants.AUTH_TYPE_ID)) {
-				user = userPersistence.findByC_U(
-					companyId, GetterUtil.getLong(login));
-			}
+		if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
+			user = fetchUserByEmailAddress(companyId, login);
 		}
-		catch (NoSuchUserException nsue) {
+		else if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
+			user = fetchUserByScreenName(companyId, login);
+		}
+		else if (authType.equals(CompanyConstants.AUTH_TYPE_ID)) {
+			user = userPersistence.fetchByPrimaryKey(GetterUtil.getLong(login));
+		}
+
+		if (user == null) {
 			return Authenticator.DNE;
 		}
 
@@ -5419,10 +5405,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 						parameterMap);
 				}
 
-				try {
-					user = userPersistence.findByPrimaryKey(user.getUserId());
-				}
-				catch (NoSuchUserException nsue) {
+				user = userPersistence.fetchByPrimaryKey(user.getUserId());
+
+				if (user == null) {
 					return Authenticator.DNE;
 				}
 

portal-impl/src/com/liferay/portal/events/SampleAppStartupAction.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.events;
 
-import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.log.Log;
@@ -51,14 +50,10 @@ public class SampleAppStartupAction extends SimpleAction {
 	}
 
 	protected void doRun(long companyId) throws Exception {
-		try {
-			UserLocalServiceUtil.getUserByScreenName(companyId, "paul");
-
-			// Do not populate the sample database if Paul already exists
+		if (UserLocalServiceUtil.fetchUserByScreenName(
+				companyId, "paul") != null) {
 
 			return;
-		}
-		catch (NoSuchUserException nsue) {
 		}
 
 		long creatorUserId = 0;

portal-impl/src/com/liferay/portal/security/auth/OpenSSOAutoLogin.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.security.auth;
 
-import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -141,17 +140,15 @@ public class OpenSSOAutoLogin extends BaseAutoLogin {
 		if (PrefsPropsUtil.getBoolean(
 				companyId, PropsKeys.USERS_SCREEN_NAME_ALWAYS_AUTOGENERATE)) {
 
-			try {
-				user = UserLocalServiceUtil.getUserByEmailAddress(
-					companyId, emailAddress);
+			user = UserLocalServiceUtil.fetchUserByEmailAddress(
+				companyId, emailAddress);
 
+			if (user != null) {
 				ScreenNameGenerator screenNameGenerator =
 					ScreenNameGeneratorFactory.getInstance();
 
 				screenName = screenNameGenerator.generate(
 					companyId, user.getUserId(), emailAddress);
-			}
-			catch (NoSuchUserException nsue) {
 			}
 		}
 
@@ -181,12 +178,8 @@ public class OpenSSOAutoLogin extends BaseAutoLogin {
 		}
 
 		if (user == null) {
-			try {
-				user = UserLocalServiceUtil.getUserByScreenName(
-					companyId, screenName);
-			}
-			catch (NoSuchUserException nsue) {
-			}
+			user = UserLocalServiceUtil.fetchUserByScreenName(
+				companyId, screenName);
 		}
 
 		if (user == null) {

portal-impl/src/com/liferay/portal/security/auth/SiteMinderAutoLogin.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.security.auth;
 
-import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringPool;
@@ -82,18 +81,13 @@ public class SiteMinderAutoLogin extends BaseAutoLogin {
 		}
 
 		if (user == null) {
-			try {
-				if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
-					user = UserLocalServiceUtil.getUserByEmailAddress(
-						companyId, siteMinderUserHeader);
-				}
-				else {
-					user = UserLocalServiceUtil.getUserByScreenName(
-						companyId, siteMinderUserHeader);
-				}
+			if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
+				user = UserLocalServiceUtil.fetchUserByEmailAddress(
+					companyId, siteMinderUserHeader);
 			}
-			catch (NoSuchUserException nsue) {
-				return null;
+			else {
+				user = UserLocalServiceUtil.fetchUserByScreenName(
+					companyId, siteMinderUserHeader);
 			}
 		}
 

portal-impl/src/com/liferay/portal/security/ldap/PortalLDAPImporterImpl.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.security.ldap;
 
 import com.liferay.portal.NoSuchRoleException;
-import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.NoSuchUserGroupException;
 import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
 import com.liferay.portal.kernel.cache.PortalCache;
@@ -564,23 +563,19 @@ public class PortalLDAPImporterImpl implements PortalLDAPImporter {
 	protected User getUser(long companyId, LDAPUser ldapUser) throws Exception {
 		User user = null;
 
-		try {
-			String authType = PrefsPropsUtil.getString(
-				companyId, PropsKeys.COMPANY_SECURITY_AUTH_TYPE,
-				PropsValues.COMPANY_SECURITY_AUTH_TYPE);
+		String authType = PrefsPropsUtil.getString(
+			companyId, PropsKeys.COMPANY_SECURITY_AUTH_TYPE,
+			PropsValues.COMPANY_SECURITY_AUTH_TYPE);
 
-			if (authType.equals(CompanyConstants.AUTH_TYPE_SN) &&
-				!ldapUser.isAutoScreenName()) {
+		if (authType.equals(CompanyConstants.AUTH_TYPE_SN) &&
+			!ldapUser.isAutoScreenName()) {
 
-				user = UserLocalServiceUtil.getUserByScreenName(
-					companyId, ldapUser.getScreenName());
-			}
-			else {
-				user = UserLocalServiceUtil.getUserByEmailAddress(
-					companyId, ldapUser.getEmailAddress());
-			}
+			user = UserLocalServiceUtil.fetchUserByScreenName(
+				companyId, ldapUser.getScreenName());
 		}
-		catch (NoSuchUserException nsue) {
+		else {
+			user = UserLocalServiceUtil.fetchUserByEmailAddress(
+				companyId, ldapUser.getEmailAddress());
 		}
 
 		return user;

portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -20,7 +20,6 @@ import com.liferay.portal.CompanyVirtualHostException;
 import com.liferay.portal.CompanyWebIdException;
 import com.liferay.portal.LocaleException;
 import com.liferay.portal.NoSuchShardException;
-import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.NoSuchVirtualHostException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -283,18 +282,16 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 		// Default user
 
-		User defaultUser = null;
+		User defaultUser = userPersistence.fetchByC_DU(companyId, true);
 
-		try {
-			defaultUser = userLocalService.getDefaultUser(companyId);
-
+		if (defaultUser != null) {
 			if (!defaultUser.isAgreedToTermsOfUse()) {
 				defaultUser.setAgreedToTermsOfUse(true);
 
 				userPersistence.update(defaultUser);
 			}
 		}
-		catch (NoSuchUserException nsue) {
+		else {
 			long userId = counterLocalService.increment();
 
 			defaultUser = userPersistence.create(userId);

portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -20,7 +20,6 @@ import com.liferay.portal.GroupNameException;
 import com.liferay.portal.GroupParentException;
 import com.liferay.portal.NoSuchGroupException;
 import com.liferay.portal.NoSuchLayoutSetException;
-import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.RequiredGroupException;
 import com.liferay.portal.kernel.cache.ThreadLocalCachable;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
@@ -3246,18 +3245,15 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 		User user = null;
 
-		try {
-			user = userPersistence.findByPrimaryKey(group.getCreatorUserId());
+		user = userPersistence.fetchByPrimaryKey(group.getCreatorUserId());
 
+		if (user == null) {
+			user = userPersistence.fetchByPrimaryKey(
+				serviceContext.getUserId());
 		}
-		catch (NoSuchUserException nsue1) {
-			try {
-				user = userPersistence.findByPrimaryKey(
-					serviceContext.getUserId());
-			}
-			catch (NoSuchUserException nsue2) {
-				user = userLocalService.getDefaultUser(group.getCompanyId());
-			}
+
+		if (user == null) {
+			user = userLocalService.getDefaultUser(group.getCompanyId());
 		}
 
 		updateAsset(

portal-impl/src/com/liferay/portal/servlet/filters/autologin/AutoLoginFilter.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.servlet.filters.autologin;
 
-import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.ProtectedServletRequest;
@@ -82,21 +81,15 @@ public class AutoLoginFilter extends BasePortalFilter {
 			return null;
 		}
 
-		try {
-			long userId = GetterUtil.getLong(jUsername);
+		long userId = GetterUtil.getLong(jUsername);
 
-			if (userId > 0) {
-				User user = UserLocalServiceUtil.getUserById(userId);
-
-				if (user.isLockout()) {
-					return null;
-				}
-			}
-			else {
-				return null;
-			}
+		if (userId <= 0) {
+			return null;
 		}
-		catch (NoSuchUserException nsue) {
+
+		User user = UserLocalServiceUtil.fetchUserById(userId);
+
+		if ((user == null) || user.isLockout()) {
 			return null;
 		}
 

commit 0469a2e8e55f1579a64366736a04fc3aa4e4fe9a

LPS-70203 Avoid duplicated login trim and toLowerCase, caused by 6c12b7b88488be357afac1842e9943c487d7af22

portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -5839,10 +5839,10 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		User user = null;
 
 		if (authType.equals(CompanyConstants.AUTH_TYPE_EA)) {
-			user = fetchUserByEmailAddress(companyId, login);
+			user = userPersistence.fetchByC_EA(companyId, login);
 		}
 		else if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
-			user = fetchUserByScreenName(companyId, login);
+			user = userPersistence.fetchByC_SN(companyId, login);
 		}
 		else if (authType.equals(CompanyConstants.AUTH_TYPE_ID)) {
 			user = userPersistence.fetchByPrimaryKey(GetterUtil.getLong(login));