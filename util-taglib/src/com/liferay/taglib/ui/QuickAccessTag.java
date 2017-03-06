fc24c1a5dd2285e473d32001df074e9d21f1b318
shuyangzhou <shuyang.zhou@liferay.com>
2017-03-06 10:37:00 -0800

    LPS-X Inline QuickAccessTag's page.jsp

--- util-taglib/src/com/liferay/taglib/ui/QuickAccessTag.java
+++ util-taglib/src/com/liferay/taglib/ui/QuickAccessTag.java
@@ -15,12 +15,20 @@
 package com.liferay.taglib.ui;
 
 import com.liferay.portal.kernel.servlet.taglib.ui.QuickAccessEntry;
+import com.liferay.portal.kernel.util.ReflectionUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.aui.ScriptTag;
 import com.liferay.taglib.util.IncludeTag;
+
+import java.io.IOException;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
 
 /**
  * @author     Eudaldo Alonso
@@ -37,18 +45,113 @@ public class QuickAccessTag extends IncludeTag {
 	}
 
 	@Override
-	protected void setAttributes(HttpServletRequest request) {
-		List<QuickAccessEntry> quickAccessEntries =
-			(List<QuickAccessEntry>)request.getAttribute(
-				WebKeys.PORTLET_QUICK_ACCESS_ENTRIES);
+	protected int processEndTag() throws Exception {
+		_quickAccessEntries = (List<QuickAccessEntry>)request.getAttribute(
+			WebKeys.PORTLET_QUICK_ACCESS_ENTRIES);
 
-		request.setAttribute("liferay-ui:quick-access:contentId", _contentId);
-		request.setAttribute(
-			"liferay-ui:quick-access:quickAccessEntries", quickAccessEntries);
+		boolean hasQuickAccessEntries = false;
+
+		if ((_quickAccessEntries != null) && !_quickAccessEntries.isEmpty()) {
+			hasQuickAccessEntries = true;
+		}
+
+		if (!hasQuickAccessEntries && Validator.isNull(_contentId)) {
+			return EVAL_PAGE;
+		}
+
+		_randomNamespace = StringUtil.randomId();
+
+		_randomNamespace = _randomNamespace.concat(StringPool.UNDERLINE);
+
+		JspWriter jspWriter = pageContext.getOut();
+
+		jspWriter.write("<nav class=\"quick-access-nav\" id=\"");
+		jspWriter.write(_randomNamespace);
+		jspWriter.write("quickAccessNav\">");
+		jspWriter.write("<h1 class=\"hide-accessible\">");
+
+		MessageTag messageTag = new MessageTag();
+
+		messageTag.setKey("navigation");
+
+		messageTag.doTag(pageContext);
+
+		jspWriter.write("</h1><ul>");
+
+		if (Validator.isNotNull(_contentId)) {
+			jspWriter.write("<li><a href=\"");
+			jspWriter.write(_contentId);
+			jspWriter.write("\">");
+
+			messageTag.setKey("skip-to-content");
+
+			messageTag.doTag(pageContext);
+
+			jspWriter.write("</a></li>");
+		}
+
+		if (hasQuickAccessEntries) {
+			for (QuickAccessEntry quickAccessEntry : _quickAccessEntries) {
+				jspWriter.write("<li><a href=\"");
+				jspWriter.write(quickAccessEntry.getURL());
+				jspWriter.write("\" id=\"");
+				jspWriter.write(_randomNamespace);
+				jspWriter.write(quickAccessEntry.getId());
+				jspWriter.write("\">");
+				jspWriter.write(quickAccessEntry.getContent());
+				jspWriter.write("</a></li>");
+			}
+		}
+
+		jspWriter.write("</ul></nav>");
+
+		if (hasQuickAccessEntries) {
+			ScriptTag scriptTag = new ScriptTag();
+
+			scriptTag.setSandbox(true);
+
+			scriptTag.doBodyTag(pageContext, this::_processScriptBody);
+		}
+
+		return EVAL_PAGE;
+	}
+
+	private void _processScriptBody(PageContext pageContext) {
+		JspWriter jspWriter = pageContext.getOut();
+
+		try {
+			jspWriter.write("var callbacks = {};");
+
+			for (QuickAccessEntry quickAccessEntry : _quickAccessEntries) {
+				String onClick = quickAccessEntry.getOnClick();
+
+				if (Validator.isNotNull(onClick)) {
+					jspWriter.write("callbacks['");
+					jspWriter.write(_randomNamespace);
+					jspWriter.write(quickAccessEntry.getId());
+					jspWriter.write("'] = function() {");
+					jspWriter.write(onClick);
+					jspWriter.write("};");
+				}
+			}
+
+			jspWriter.write("$('#");
+			jspWriter.write(_randomNamespace);
+			jspWriter.write(
+				"quickAccessNav').on('click','li a',function(event) {");
+			jspWriter.write("var callbackFn = ");
+			jspWriter.write("callbacks[$(event.currentTarget).attr('id')];");
+			jspWriter.write("if (_.isFunction(callbackFn)) {callbackFn();}});");
+		}
+		catch (IOException ioe) {
+			ReflectionUtil.throwException(ioe);
+		}
 	}
 
 	private static final String _PAGE = "/html/taglib/ui/quick_access/page.jsp";
 
 	private String _contentId;
+	private List<QuickAccessEntry> _quickAccessEntries;
+	private String _randomNamespace;
 
 }
