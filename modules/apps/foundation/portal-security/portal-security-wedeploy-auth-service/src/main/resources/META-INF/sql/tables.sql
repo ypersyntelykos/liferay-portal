a9b69a5b6c93dae881707f9fd474b65475b805a8
Ryan Park <ryan.park@liferay.com>
2017-04-13 17:34:58 -0700

    LRIS-25643 Generated

---
+++ modules/apps/foundation/portal-security/portal-security-wedeploy-auth-service/src/main/resources/META-INF/sql/tables.sql
@@ -0,0 +1,11 @@
+create table WedeployAuth_WeDeployAuthApp (
+	weDeployAuthAppId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	name VARCHAR(75) null,
+	clientId VARCHAR(75) null,
+	clientSecret VARCHAR(75) null
+);
