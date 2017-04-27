681a5f29a1f03ab95016f44f0011186d24506d44
Preston-Crary <preston.crary@liferay.com>
2017-04-27 14:05:10 -0700

    LPS-72240 Extract method.

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/display/context/AssetPublisherDisplayContext.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/display/context/AssetPublisherDisplayContext.java
@@ -609,28 +609,16 @@ public class AssetPublisherDisplayContext {
 			return Collections.emptyMap();
 		}
 
-		Map<Long, Map<String, PortletURL>> scopeAddPortletURLs = new HashMap();
-
-		LiferayPortletResponse liferayPortletResponse =
-			(LiferayPortletResponse)_portletResponse;
-
-		PortletURL redirectURL = liferayPortletResponse.createRenderURL();
-
-		redirectURL.setParameter(
-			"hideDefaultSuccessMessage", Boolean.TRUE.toString());
-		redirectURL.setParameter("mvcPath", "/add_asset_redirect.jsp");
+		Map<Long, Map<String, PortletURL>> scopeAddPortletURLs =
+			new HashMap<>();
 
 		LiferayPortletRequest liferayPortletRequest =
 			(LiferayPortletRequest)_portletRequest;
+		LiferayPortletResponse liferayPortletResponse =
+			(LiferayPortletResponse)_portletResponse;
 
-		PortletURL currentURLObj = PortletURLUtil.getCurrent(
+		String redirect = _getScopeAssetPortletRedirect(
 			liferayPortletRequest, liferayPortletResponse);
-
-		redirectURL.setParameter("redirect", currentURLObj.toString());
-
-		redirectURL.setWindowState(LiferayWindowState.POP_UP);
-
-		String redirect = redirectURL.toString();
 
 		for (long groupId : groupIds) {
 			Map<String, PortletURL> addPortletURLs =
@@ -1207,6 +1195,27 @@ public class AssetPublisherDisplayContext {
 
 			_ddmStructureFieldLabel = classTypeField.getLabel();
 		}
+	}
+
+	private String _getScopeAssetPortletRedirect(
+			LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws Exception {
+
+		PortletURL redirectURL = liferayPortletResponse.createRenderURL();
+
+		redirectURL.setParameter(
+			"hideDefaultSuccessMessage", Boolean.TRUE.toString());
+		redirectURL.setParameter("mvcPath", "/add_asset_redirect.jsp");
+
+		PortletURL currentURLObj = PortletURLUtil.getCurrent(
+			liferayPortletRequest, liferayPortletResponse);
+
+		redirectURL.setParameter("redirect", currentURLObj.toString());
+
+		redirectURL.setWindowState(LiferayWindowState.POP_UP);
+
+		return redirectURL.toString();
 	}
 
 	private Integer _abstractLength;
